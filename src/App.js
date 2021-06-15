e57e2866be19ef656dc9dc9d34d1264c815930af
akulsr0 <akulsr0@gmail.com>
2021-06-16 00:25:30 +0530

    chore: init app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import { startTransition, useEffect, useState } from "react";
+import { unstable_batchedUpdates } from "react-dom";
+import "./App.css";
 
 function App() {
+  // Start Transition
+  const [search, setSearchText] = useState("");
+  const [names, setNames] = useState([]);
+  const [filteredNames, setFilteredNames] = useState([]);
+
+  // Batching
+  const [count, setCount] = useState(0);
+  const [isDark, setIsDark] = useState(true);
+  console.count("ReRender: ");
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?inc=name&results=100")
+      .then((res) => res.json())
+      .then(({ results }) => {
+        setNames(results);
+      });
+  }, []);
+
+  const onChangeSearch = (e) => {
+    e.preventDefault();
+    const searchText = e.target.value;
+
+    // 1st Priority
+    setSearchText(setSearchText);
+    if (searchText === "") return setFilteredNames([]);
+
+    // 2nd Priority
+    startTransition(() => {
+      const results = names.filter(({ name }) =>
+        name.first.toLowerCase().startsWith(searchText)
+      );
+      setFilteredNames(results);
+    });
+  };
+
+  const changeWithoutBatching = () => {
+    setCount((prev) => prev + 1); // no rerender
+    setIsDark((prev) => !prev); // no rerender
+    // rerenders
+  };
+
+  const changeWithoutBatchingAPI = () => {
+    setTimeout(() => {
+      setCount((prev) => prev + 1); // rerender
+      setIsDark((prev) => !prev); // rerender again
+    });
+  };
+
+  const changeWithBatching = () => {
+    setTimeout(() => {
+      unstable_batchedUpdates(() => {
+        setCount((prev) => prev + 1);
+        setIsDark((prev) => !prev);
+        // rerenders
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>React 18 New Features</h2>
+      <br />
+      <h3>setTransition</h3>
+      <br />
+      <input
+        defaultValue={search}
+        onChange={onChangeSearch}
+        placeholder={"Search"}
+      />
+      <ul>
+        {filteredNames.map((n, i) => (
+          <li key={i}>{n.name.first}</li>
+        ))}
+      </ul>
+      <br />
+      <hr />
+      <br />
+      <h3>Batching</h3>
+      <small>Open console for rerender count</small>
+      <br />
+      <br />
+      <p
+        style={{
+          backgroundColor: isDark ? "#000" : "#fff",
+          color: isDark ? "#fff" : "#000",
+        }}
+      >
+        Number: {count}
+      </p>
+      <br />
+      <button onClick={changeWithoutBatching}>Without Batching</button>
+      <pre>
+        {`const changeWithoutBatching = () => {
+  setCount((prev) => prev + 1); // no rerender
+  setIsDark((prev) => !prev); // no rerender
+  // rerenders
+};`}
+      </pre>
+      <p>
+        This will cause <b>one</b> rerender and <b>two</b> state changes
+      </p>
+      <br />
+      <button onClick={changeWithoutBatchingAPI}>
+        Without Batching (API/Promise)
+      </button>
+      <pre>
+        {`const changeWithoutBatchingAPI = () => {
+  setTimeout(() => {
+    setCount((prev) => prev + 1); // rerender
+    setIsDark((prev) => !prev); // rerender again
+  });
+}`}
+      </pre>
+      <p>
+        This will cause <b>two</b> rerender and <b>two</b> state changes
+      </p>
+      <br />
+      <button onClick={changeWithBatching}>
+        With Batching (unstable_batchedUpdates)
+      </button>
+      <pre>
+        {`const changeWithBatching = () => {
+  setTimeout(() => {
+    unstable_batchedUpdates(() => {
+      setCount((prev) => prev + 1);
+      setIsDark((prev) => !prev);
+      // rerenders
+    });
+  });
+};`}
+      </pre>
+      <p>
+        This will cause <b>one</b> rerender and <b>two</b> state changes
+      </p>
+      <hr />
+    </>
   );
 }
 
